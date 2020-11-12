e0652de6c5e3455ec9091c6f62b00a0e576985ad
foadolfat <olfatfoad@gmail.com>
2020-11-11 16:30:31 -0800

    First commit Foad

--- weather/src/test.java
+++ weather/src/test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args){
         System.out.println("Helloo, please work ughhhhh");
+        System.out.println("Helloo, please work ughhhhh");
     }
 }
