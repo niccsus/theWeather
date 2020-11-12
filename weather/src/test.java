c097068bf119bce263dd55fa3d82c64d3b56b856
Victor Peraza <69927217+Victor-Peraza@users.noreply.github.com>
2020-11-11 16:35:50 -0800

    it's vic

--- weather/src/test.java
+++ weather/src/test.java
@@ -1,6 +1,8 @@
 public class test {
     public static void main(String[] args){
+
         System.out.println("Helloo, please work ughhhhh");
         System.out.println("Helloo, please work ughhhhh");
+        System.out.println("It's Vic");
     }
 }
