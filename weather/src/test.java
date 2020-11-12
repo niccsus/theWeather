7df3b68198b7f2fc17109b1de65381a7ed10a8e3
harpreetpadda <paddaharpreet29@yahoo.com>
2020-11-11 16:41:27 -0800

    Commit

--- weather/src/test.java
+++ weather/src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args){
         System.out.println("Helloo, please work ughhhhh");
         System.out.println("Helloo, please work ughhhhh");
+        System.out.println("Harpreet Tried");
     }
 }
