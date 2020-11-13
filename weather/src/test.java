ea7c9120cbd576e82005c361a30989e17dbf30fc
harpreetpadda <paddaharpreet29@yahoo.com>
2020-11-12 19:11:29 -0800

    did it work

--- weather/src/test.java
+++ weather/src/test.java
@@ -2,7 +2,10 @@ public class test {
     public static void main(String[] args){
         System.out.println("Helloo, please work ughhhhh");
         System.out.println("Helloo, please work ughhhhh");
+        System.out.println("did it work?");
     }
 }
  //Jaishil
  //update thiis branch
+ //did it work
+ //change
