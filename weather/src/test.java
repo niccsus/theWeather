53f9514866b959fd1f311420af456f5acfd47399
foadolfat <olfatfoad@gmail.com>
2020-11-12 18:51:55 -0800

    Foad second comit to my branch

--- weather/src/test.java
+++ weather/src/test.java
@@ -3,6 +3,7 @@ public class test {
         System.out.println("Helloo, please work ughhhhh");
         System.out.println("Helloo, please work ughhhhh");
         System.out.println("Helloo, please work ughhhhh");
+        New_Class_Test.testing_classes();
 
     }
 }
