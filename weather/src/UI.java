00c1e4a51c3764f8a4291a8f11becb8b3f3707e7
Victor Peraza <69927217+Victor-Peraza@users.noreply.github.com>
2020-11-16 14:29:06 -0800

    Built Frame

--- weather/src/UI.java
+++ weather/src/UI.java
@@ -1,15 +1,12 @@
 import javax.swing.*;  
 public class UI {  
 public static void main(String[] args) {  
-JFrame frame =new JFrame();//
-          
-JButton button =new JButton("Check Weather");//
-button.setBounds(100,5,200, 50);//x axis, y axis, width, height  
-          
-frame.add(button);
-          
-frame.setSize(400,500);
-frame.setLayout(null);
-frame.setVisible(true);//makes frame
-}  
-}  
+JFrame frame = new JFrame();
+JButton button = new JButton("The Weather");
+
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.getContentPane().add(button);
+frame.setSize(300,300);
+frame.setVisible(true);
+}
+}
