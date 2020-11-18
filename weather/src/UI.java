47f8daa84ff8c201d488c0ee86d863823a1596b7
Victor Peraza <69927217+Victor-Peraza@users.noreply.github.com>
2020-11-17 20:11:51 -0800

    Added a background for the GUI

--- weather/src/UI.java
+++ weather/src/UI.java
@@ -1,13 +1,14 @@
 import javax.swing.*;  
+import java.awt.*;
 
 public class UI {  
     public static void frame() {
-        JFrame frame = new JFrame();
-        JButton button = new JButton("The Weather");
+        JFrame frame = new JFrame("The Weather");
+       // JButton button = new JButton("The Weather");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(button);
-        frame.setSize(300,300);
+        frame.add(new JLabel(new ImageIcon("/Users/victorperaza/git/theWeather/theWeather/weather/src/pic.jpg"))); //adds picture(change file path for it to work)
+        frame.setSize(1500,1000);
         frame.setVisible(true);
     }
 }
