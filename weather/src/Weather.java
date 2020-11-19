46dad7a5448f2cc462b4ddf71bbd20f88d9e6152
Victor Peraza <69927217+Victor-Peraza@users.noreply.github.com>
2020-11-18 21:15:56 -0800

    merge

--- weather/src/Weather.java
+++ weather/src/Weather.java
@@ -10,16 +10,6 @@ public class Weather {
     public static void begin() throws IOException  
     { 
         UI.frame(); //calls the frame method
-<<<<<<< HEAD
-        System.out.println("Enter City Name: ");
-        Forecast forecast = new Forecast();
-        System.out.println(forecast.get_temp());
-        System.out.println(forecast.get_cloud());
-
-        
-        
-=======
->>>>>>> 44b44240312b34bad367d481272101d4986c7c69
         
 
     } 
