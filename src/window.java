31fd69db91c562c44868a165340403cd7f89b71f
Victor Peraza <69927217+Victor-Peraza@users.noreply.github.com>
2020-11-25 20:54:45 -0800

    GUI

---
+++ src/window.java
@@ -0,0 +1,89 @@
+import javax.swing.*;  
+import java.awt.*;
+
+public class window {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					window window = new window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+	
+		/***************WINDOW FRAME************************/
+		
+		frame = new JFrame();
+		frame.setTitle("The Weather");
+		frame.setBackground(Color.BLACK);
+		frame.setForeground(Color.WHITE);
+		frame.setBounds(100, 100, 800, 430);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
+		
+		
+		/***************SEARCH BUTTON**********************/
+		
+		JButton button = new JButton("Search");
+		button.setForeground(new Color(47, 79, 79));
+		button.setBackground(new Color(100, 149, 237)); //BUTTON COLOR
+		button.setBounds(632, 337, 117, 50);			//BUTTON PLACEMENT
+		frame.getContentPane().add(button);				//ADDING BUTTTON
+		
+		/**************CELSIUS RADIO BUTTON******************/
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Celsius");
+		rdbtnNewRadioButton.setForeground(new Color(230, 230, 250));
+		rdbtnNewRadioButton.setBounds(632, 273, 141, 23);	//RADIO BUTTON PLACEMENT
+		frame.getContentPane().add(rdbtnNewRadioButton);	//ADDS RADIO BUTTON
+		
+		/***************FAHRENHEIT RADIO BUTTON***************/
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Fahrenheit ");
+		rdbtnNewRadioButton_1.setForeground(new Color(230, 230, 250));
+		rdbtnNewRadioButton_1.setBounds(632, 308, 141, 23);	//RADIO BUTTON PLACEMENT
+		frame.getContentPane().add(rdbtnNewRadioButton_1);	//ADDS RADIO BUTTON
+		
+		/***************TEXTFIELD AREA**********************/
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.CENTER);		//TEXT PLACEMENT
+		textField.setFont(new Font("Lucida Grande", Font.PLAIN, 21));	//FONT
+		textField.setBounds(106, 342, 514, 45);							//TEXTFIELD PLACEMENT
+		frame.getContentPane().add(textField);							//ADDS TEXTFIELD
+		textField.setColumns(10);
+		textField.setBackground(UIManager.getColor("Button.highlight"));
+
+		/****************BACKGROUND IMAGE******************/
+		
+		JLabel lblNewLabel = new JLabel("");
+		Image picture = new ImageIcon(this.getClass().getResource("/night.gif")).getImage();	//SETS IMAGE
+		lblNewLabel.setIcon(new ImageIcon(picture));
+		lblNewLabel.setBounds(0, 0, 800, 405);													//IMAGE PLACEMENT
+		frame.getContentPane().add(lblNewLabel);
+	}
+}
