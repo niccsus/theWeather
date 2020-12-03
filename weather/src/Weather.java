ed25211f6bcb335c6aa8989f644f8347c21ce183
dannyboy <danielperez2@csus.edu>
2020-12-02 17:37:19 -0800

    Added a comment

--- weather/src/Weather.java
+++ weather/src/Weather.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Weather {
     public static void main(String[] args) throws IOException {
-        UI.frame();
+        UI.frame(); //Opens the UI to run program
     }
     
 }
