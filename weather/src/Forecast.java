e8e085a03ad602aa86123a6389ecbfafb822be3f
foadolfat <olfatfoad@gmail.com>
2020-12-07 15:47:38 -0800

    Final push

--- weather/src/Forecast.java
+++ weather/src/Forecast.java
@@ -12,7 +12,6 @@ import javax.swing.JLabel;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
 public class Forecast implements Weather_Data{
     private JSONArray daily;    
     private double temp_day,temp_night,feel_day,feel_night,windspeed,temp_min,temp_max,temp_eve,temp_morn,feel_eve,feel_morn;
@@ -26,13 +25,6 @@ public class Forecast implements Weather_Data{
     JLabel temp_max_label = new JLabel("");
 
     public Forecast(){
-
-public class Forecast {
-    private JSONObject json;
-    private JSONArray daily;
-    private double temp_day,temp_night,feel_day,feel_night,windspeed;
-    private int cloudPercentage;
-
 
     }
  
@@ -152,7 +144,6 @@ public class Forecast {
         return icon;
     }
 
-
     public double get_temp_min(){
         return temp_min;
     }
@@ -223,43 +214,3 @@ public class Forecast {
 
     
 }
-
-    public Forecast(JSONObject json, int day){
-        this.json = json;
-        this.daily = (JSONArray) json.get("daily");
-        JSONObject currentObj = (JSONObject) daily.get(day);
-        this.temp_day = currentObj.getJSONObject("temp").getDouble("day");
-        this.temp_night = currentObj.getJSONObject("temp").getDouble("night");
-        this.feel_day = currentObj.getJSONObject("feels_like").getDouble("night");
-        this.feel_night = currentObj.getJSONObject("feels_like").getDouble("day");
-        this.windspeed = currentObj.getDouble("wind_speed");
-        this.cloudPercentage = currentObj.getInt("clouds");
-
-        System.out.println(temp_day);
-    }
-
-    public double getTemp_day() {
-        return temp_day;
-    }
-
-    public double getTemp_night() {
-        return temp_night;
-    }
-
-    public double getFeel_day() {
-        return feel_day;
-    }
-
-    public double getFeel_night() {
-        return feel_night;
-    }
-
-    public double getWindspeed() {
-        return windspeed;
-    }
-
-    public int getCloudPercentage() {
-        return cloudPercentage;
-    }
-}
-
